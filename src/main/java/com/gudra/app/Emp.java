59c48e8522116eb37f3457be5e37f1ac32054b9b
ashrithaps <ashi.831990@gmail.com>
2016-11-09 17:09:52 +0530

    hibernate -mysql mappings with Employee junit Test class

--- src/main/java/com/gudra/app/Emp.java
+++ src/main/java/com/gudra/app/Emp.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table (name = "Employee")
+@Table (name = "employee")
 public class Emp {
     @Id
     private String Name;
---
+++ src/test/java/com/gudra/app/EmployeeClassTest.java
@@ -0,0 +1,88 @@
+package com.gudra.app;
+
+import com.gudra.app.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+import java.util.List;
+
+/**
+ * Created by Ashritha on 11/9/2016.
+ */
+
+public class EmployeeClassTest {
+   @Test
+    public void saveEmployeeDetailsTest(){
+       SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+       Session session = sessionFactory.openSession();
+       session.beginTransaction();
+
+       Emp employee =new Emp();
+       employee.setName("Sesu");
+       employee.setSalary(500);
+       session.save(employee);
+       session.getTransaction().commit();
+
+       session.close();
+
+   }
+
+   @Test
+   public void listAllEmployeesTest(){
+      SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+      Session session = sessionFactory.openSession();
+      session.beginTransaction();
+
+      List<Emp> employees=session.createQuery("from Emp").list();
+      session.getTransaction().commit();
+
+      session.close();
+      Assert.assertEquals(3, employees.size());
+   }
+
+   @Test
+   public void getEmployeeDetailsTest(){
+      SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+      Session session = sessionFactory.openSession();
+      session.beginTransaction();
+
+      Emp empDetails = (Emp) session.get(Emp.class, "Sesu");
+      session.getTransaction().commit();
+
+      session.close();
+      Assert.assertEquals(550, empDetails.getSalary());
+   }
+
+   @Test
+   public void updateEmployeeDetailsTest(){
+      SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+      Session session = sessionFactory.openSession();
+      session.beginTransaction();
+
+
+      Emp employee= (Emp)session.createQuery("from Emp where Name='Asha'").uniqueResult();
+      employee.setSalary(400);
+      session.update(employee);
+      session.getTransaction().commit();
+
+      session.close();
+   }
+
+
+   @Test
+   public void deleteEmployeeTest(){
+      SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+      Session session = sessionFactory.openSession();
+      session.beginTransaction();
+
+
+      Emp employee= (Emp)session.createQuery("from Emp where Name='Sesu'").uniqueResult();
+      session.delete(employee);
+      session.getTransaction().commit();
+
+      session.close();
+   }
+}
