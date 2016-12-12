5df828ad5ede92be1073fdb06a239feadb92cf06
ashrithaps <ashi.831990@gmail.com>
2016-12-12 17:43:49 +0530

    image filed added

--- src/main/java/com/gudra/app/Emp.java
+++ src/main/java/com/gudra/app/Emp.java
@@ -2,7 +2,11 @@ package com.gudra.app;
 
 /**
  * Created by Ashritha on 10/28/2016.
+ * Employee class containing two member variables Name and Salary
+ * It is annotated using JPA hibernate annotations where Name is ID or Primary key for table employee
  */
+import org.hibernate.annotations.Type;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -12,7 +16,18 @@ public class Emp {
     @Id
     private String Name;
     private int Salary;
+    private String address;
+    private long contactNo;
+    @Lob
+    private byte[] image;
 
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
 
     public String getName() {
         return Name;
@@ -22,8 +37,6 @@ public class Emp {
         Name = name;
     }
 
-
-
     public int getSalary() {
         return Salary;
     }
@@ -31,6 +44,20 @@ public class Emp {
     public void setSalary(int salary) {
         Salary = salary;
     }
+    public String getAddress() {
+        return address;
+    }
 
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public long getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(long contactNo) {
+        this.contactNo = contactNo;
+    }
 
 }
